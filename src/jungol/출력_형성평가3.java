0d89cc3f2e64a4a0e5ea869b816c0a4e87fd1610
kiwoong Park <pgw10243@gmail.com>
2025-07-23 15:47:18 +0900

    정올 연습문제 풀이 07.23(수)

---
+++ src/jungol/출력_형성평가3.java
@@ -0,0 +1,11 @@
+package jungol;
+
+public class 출력_형성평가3 {
+    public static void main(String[] args) {
+        System.out.println("TTTTTTTTTT\n" +
+                "TTTTTTTTTT\n" +
+                "    TT\n" +
+                "    TT\n" +
+                "    TT");
+    }
+}
---
+++ src/jungol/출력_형성평가4.java
@@ -0,0 +1,10 @@
+package jungol;
+
+public class 출력_형성평가4 {
+    public static void main(String[] args) {
+        System.out.println("kor 90\n" +
+                "mat 80\n" +
+                "eng 100\n" +
+                "sum 270");
+    }
+}
