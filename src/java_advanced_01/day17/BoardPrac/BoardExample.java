449b13195bae7c32719af60f6ae75cc7c03dd6bc
kiwoongPark <pgw10243@gmail.com>
2025-08-19 23:36:45 +0900

    Board practice 기능 로직 구현 완료 및 내용 확인 프린트문 수정 필요

--- src/java_advanced_01/day17/BoardPrac/BoardExample.java
+++ src/java_advanced_01/day17/BoardPrac/BoardExample.java
@@ -1,5 +1,7 @@
 package java_advanced_01.day17.BoardPrac;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class BoardExample {
@@ -18,7 +20,6 @@ public class BoardExample {
         boardList.add(new Board(Bno++, "눈 오는 날", "컨텐츠", "winter", new Date()));
         boardList.add(new Board(Bno++, "크리스마스", "컨텐츠", "winter", new Date()));
         boardList.add(new Board(Bno++, "여름에 가장 시원할 때", "컨텐츠", "summer", new Date()));
-
     }
 
     void list() { // 게시물 목록을 출력하고 mainMenu()를 실행한다.
@@ -26,14 +27,14 @@ public class BoardExample {
                 """
                 [게시물 목록]
                 ------------------------------------------------------------
-                no\twriter\t\tdate\t\ttitle\t\t
+                no\twriter\t\tdate\t\t\ttitle\t\t
                 ------------------------------------------------------------
                 """);
 
-
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         for (Board board : boardList) {
-            System.out.printf("%d\t%-20s\t\t-20s\t\t-20s\t\t\n",
-                    board.getBno(), board.getBwriter(), board.getBdate(), board.getBtitle());
+            System.out.printf("%d\t%10s\t\t%10s\t\t%10s\n",
+                    board.getBno(), board.getBwriter(), sdf.format(board.getBdate()), board.getBtitle());
         }
         System.out.println();
 
@@ -45,30 +46,17 @@ public class BoardExample {
                 """
                 메인 메뉴 : 1.Create  | 2. Read | 3.Clear | 4.Exit
                 """);
-        int choiceNum = 0;
-        boolean validInput = false;
-
-        // 올바른 입력이 들어올 때까지 반복합니다.
-        while(!validInput) {
-            try {
-                System.out.println("메뉴 선택 : ");
-                choiceNum = Integer.parseInt(sc.nextLine());
-                if (choiceNum < 1 || choiceNum > 4) {
-                    System.out.println("1부터 4까지만 입력해주세요.");
-                    continue;
-                }
-                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
-                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
-            }
-        }
+        int choiceNum = validCheck(1, 4);
 
         switch (choiceNum) {
             case 1 -> create();
             case 2 -> read();
             case 3 -> clear();
-            case 4 -> System.out.println("프로그램을 종료합니다.");
+            case 4 -> {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
         }
     }
 
@@ -89,22 +77,8 @@ public class BoardExample {
         System.out.println("--------------------------------------------");
         System.out.println("1. OK | 2. Cancel");
         System.out.println("메뉴 선택 : ");
-        boolean validInput = false;
-        int menuNum = 0;
-        // 올바른 입력이 들어올 때까지 반복합니다.
-        while(!validInput) {
-            try {
-                menuNum = Integer.parseInt(sc.nextLine());
-                if (menuNum < 1 || menuNum > 2) {
-                    System.out.println("1부터 2까지만 입력해주세요.");
-                    continue;
-                }
-                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
-                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
-            }
-        }
+
+        int menuNum = validCheck(1, 2);
 
         switch (menuNum) {
             case 1 -> boardList.add(new Board(Bno++,title, content, writer, new Date()));
@@ -113,28 +87,21 @@ public class BoardExample {
     }
 
     void read() {
-        int choiceBno = 0;
+
         System.out.println("[게시물 읽기]");
         System.out.println("bno : ");
 
-        boolean validInput = false;
-        // 올바른 입력이 들어올 때까지 반복합니다.
-        while(!validInput) {
-            try {
-                choiceBno = Integer.parseInt(sc.nextLine());
-                if (choiceBno < 1 || choiceBno > Bno) {
-                    System.out.printf("1부터 %d까지만 입력해주세요.\n",Bno);
-                    continue;
-                }
-                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
-                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
-            }
-        }
+        int choiceBno = validCheck(1, Bno);
+
         System.out.println("#######################");
         // 게시물 내용 출력
-        System.out.println(boardList.get(choiceBno).toString());
+        for (Board board:boardList) {
+            if (choiceBno == board.getBno()) {
+                System.out.println(board.toString());
+                break;
+            }
+        }
+
         System.out.println("#######################");
         submenu(choiceBno);
 
@@ -143,22 +110,8 @@ public class BoardExample {
 
     void submenu(int choiceBno) {
         System.out.println("보조 메뉴 : 1.Update | 2.Delete | 3.List");
-        int choiceNum = 0;
-        boolean validInput = false;
-        // 올바른 입력이 들어올 때까지 반복합니다.
-        while(!validInput) {
-            try {
-                choiceNum = Integer.parseInt(sc.nextLine());
-                if (choiceNum < 1 || choiceNum > 3) {
-                    System.out.printf("1부터 3까지만 입력해주세요.\n");
-                    continue;
-                }
-                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
-                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
-            }
-        }
+
+        int choiceNum = validCheck(1, 3);
 
         switch(choiceNum) {
             case 1 -> update(choiceBno);
@@ -185,22 +138,8 @@ public class BoardExample {
         System.out.println("--------------------------------------------");
         System.out.println("1. OK | 2. Cancel");
         System.out.println("메뉴 선택 : ");
-        boolean validInput = false;
-        int menuNum = 1;
-        // 올바른 입력이 들어올 때까지 반복합니다.
-        while(!validInput) {
-            try {
-                menuNum = Integer.parseInt(sc.nextLine());
-                if (menuNum < 1 || menuNum > 2) {
-                    System.out.println("1부터 2까지만 입력해주세요.");
-                    continue;
-                }
-                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
-                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
-            }
-        }
+
+        int menuNum = validCheck(1, 2);
 
         switch (menuNum) {
             case 1 -> {
@@ -233,5 +172,25 @@ public class BoardExample {
     void clear() {
         boardList = new ArrayList<>();
         list();
+    }
+
+    int validCheck(int low, int high) {
+        int choiceNum = 0;
+        boolean validInput = false;
+        // 올바른 입력이 들어올 때까지 반복합니다.
+        while(!validInput) {
+            try {
+                choiceNum = Integer.parseInt(sc.nextLine());
+                if (choiceNum < low || choiceNum > high) {
+                    System.out.printf("%d부터 %d까지만 입력해주세요.",low,high);
+                    continue;
+                }
+                validInput = true; // 유효한 입력이 들어오면 루프를 종료합니다.
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해 주세요.");
+                sc.nextLine(); // 버퍼에 남아있는 잘못된 입력을 비워줍니다.
+            }
+        }
+        return choiceNum;
     }
 }
